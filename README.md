5914c2710e9f181e77e617c8fd604d24703d6ade
Collin Quiason Rottinghaus <collinrottinghaus@gmail.com>
2022-05-17 02:56:07 -0500

    initial commit, functioning api with ./api/ping endpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+This is an express API template
+
+Default route is ./api/...
+
+Example resource for ping on ./api/ping
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = require('./routes');
+
+// Config
+const port = process.env.PORT || 8080;
+
+// Initializations
+const expressServer = express();
+
+// Default routes
+expressServer.use('/api', routes);
+expressServer.use((err, req, res, next) => {
+    res.status(err.status || 400).json({
+        success: false,
+        message: err.message || 'An error occured.',
+        errors: err.error || [],
+    });
+});
+expressServer.use((req, res) => {
+    res.status(404).json({ success: false, message: 'Resource not found.' });
+});
+
+// Start
+expressServer.listen(port);
+
+console.log(`Server started on port ${port}`);
